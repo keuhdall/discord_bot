52f411c4871d55c341a0bf7eed85cb2ff667a32a
keuhdall <lmarques@student.42.fr>
2017-04-16 22:40:22 +0000

    added some simple features

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
 const config = require('./config.js');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const kraive = '94011401940504576';
+const fica = '166226448598695936';
+const poop = '303239764595965952';
 
 bot.on("ready", function () {
 	console.log("Ready to begin! Serving in " + bot.channels.length + " channels");
@@ -11,10 +14,24 @@ bot.on("disconnected", function () {
 	process.exit(1);
 });
 
-bot.on("message", function (msg) {
-	if (msg.content === 'ping') {
-		msg.channel.sendMessage("pong!");
-		console.log("pong-ed " + msg.author.username);
-	}
+bot.on('guildMemberAdd', ({user, guild}) => {
+	guild.defaultChannel.sendMessage(`Yo <@${user.id}>, tiens toi à carreau petit fdp sinon la PLS sera pour toi gros batard.`)
 });
+
+bot.on('guildMemberRemove', ({user, guild}) => {
+	guild.defaultChannel.sendMessage(`Cette sous race de <@${user.id}> viens de se faire mettre en PLS car il ne méritait pas de nous cotoyer, on a un haut standing ici.`)
+});
+
+bot.on("message", ({content, channel}) => {
+	if (content !== "!nogord") return ;
+	channel.sendMessage(`Qui n'a pas down Nogord ? <@${kraive}>`)
+});
+
+bot.on("message", message => {
+	if (message.author.id === fica)
+		message.react(bot.emojis.find('name', 'poop')).catch(console.error)
+	else if (message.author.id === kraive)
+		message.react(message.guild.emojis.find('name', 'nogpls')).catch(console.error)
+});
+
 bot.login(config.token);
