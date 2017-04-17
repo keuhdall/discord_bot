5ea70a7d6a32ac21ab1ebafb1b5a1ef8dc4130ae
keuhdall <lmarques@student.42.fr>
2017-04-17 16:21:38 +0000

    added !clean function ; the -c option is the only one working for now

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const bot = new Discord.Client();
 const kraive = '94011401940504576';
 const fica = '166226448598695936';
 const keuhdall = '100335365998538752';
+const navet = '94045969099792384';
 
 bot.on("ready", function () {
 	console.log("Ready to begin! Serving in " + bot.channels.length + " channels");
@@ -27,6 +28,7 @@ bot.on("message", ({content, channel}) => {
 		channel.sendMessage(`Voici la liste des commandes :\`\`\`
 - !help : affiche ce message
 - !about : donne des informations sur le bot
+- !clean [-c -t]: permet de clean les derniers messages	du channel courant. -c = count -t = time
 - !nogord : met kraive en PLS\`\`\``);
 	else if (content === "!about")
 		channel.sendMessage(`Bot fait avec amour par <@${keuhdall}>, n'hesitez pas a me contacter pour plus de renseignements`);
@@ -34,18 +36,31 @@ bot.on("message", ({content, channel}) => {
 		channel.sendMessage(`Qui n'a pas down Nogord ? <@${kraive}>`);
 });
 
-bot.on("message", ({author, channel, content}) {
-		var tab = content.split(" ");
-		if (tab[0] !== "!cleanmsg") return ;
-		if (tab[1] === "-count" && tab[2]) {
-			var col = channel.fetchMessages({limit : tab[2]});
-			for (i = 0; i < col.length, i++) {
-				col[i].delete();
+/*
+Function that clean messages;
+Available options : -count -time
+*/
+bot.on("message", message => {
+		var tab = message.content.split(" ");
+		if (tab[0] !== "!clean") return ;
+		if (tab[1] === "-c" && tab[2]) {
+			if (message.author.id !== navet && message.author.id !== keuhdall) {
+				message.channel.sendMessage('Hep hep hep, t\'as pas les droits sale retard.');
+				return ;
 			}
-			channel.sendMessage('ggwp');
+			if (tab[2] > 50) tab[2] = 50; 
+			tab[2]++;
+			message.channel.fetchMessages({limit : tab[2]})
+			.then(messages => {
+				var msg = messages.array();
+				for (var i = 0; i < tab[2]; i++)
+					msg[i].delete();
+			})
+			.catch(console.error());
+			message.channel.sendMessage(`${tab[2] - 1} Messages effacés avec succès par ${message.author.username}.`);
 		}
 		else
-			channel.sendMessage('blablabla c pa b1');
+			message.channel.sendMessage('Erreur de syntaxe dans la commande, tapez !help pour plus d\'informations');
 });
 
 bot.on("message", message => {
