f3b55df3fa5814c8b999ffa0acdfca10af03e839
Léonard Marques <lmarques@student.42.fr>
2017-12-17 04:22:13 +0100

    Fix on listener

--- apis.js
+++ apis.js
@@ -235,14 +235,14 @@ Command : !cat
                             }]
                         }});
                     }
-                }).on('error', res_err => {
-                    console.log(res_err);
-                    message.channel.send("Pas de résultat trouvé !");
-                    return;
                 });
             } catch (e) {
                 console.error(e);
             }
+        }).on('error', res_err => {
+            console.log(res_err);
+            message.channel.send("Pas de résultat trouvé !");
+            return;
         });
     },
 
